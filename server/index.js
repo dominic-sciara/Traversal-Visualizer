7e0bb337d39ea6d6ef855851cff909fd2ad4bb66
Dominic Sciara <dominic.sciara@gmail.com>
2020-01-07 16:27:09 -0500

    bug fix in the server

--- server/index.js
+++ server/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const server = require("http").createServer(app);
 
-server.listen(1337);
+server.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 
 app.use(express.static(__dirname + "/../client/public"));
 
